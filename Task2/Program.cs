8754e4ef233be37bd565f3a2be6a5e36a0b1f998
unknown <excaliburbmw@mail.ru>
2022-07-03 14:34:55 +0200

    add task2

---
+++ Task2/Program.cs
@@ -0,0 +1,79 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Console.WriteLine("Введите количество строк m: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов n: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[m, n];
+
+FillArrayRandomNumbers(array);
+Console.WriteLine();
+PrintArray(array);
+
+Console.WriteLine();
+int index = rowNumber(array);
+Console.WriteLine($"Наименьшая сумма элементов в строке номер: {index}");
+
+Console.WriteLine();
+PrintSumRow(array); 
+
+int rowNumber(int[,] array)
+{
+    int index = 1;
+    int sum = 0;
+    int result = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+        if (i == 0) result = sum;
+        else if (sum < result)
+        {
+            result = sum;
+            index = i;
+        }
+    }
+    return index;
+}
+
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(10, 99);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void PrintSumRow(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {   int sum = 0;
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+        sum = sum/1;
+        Console.WriteLine($"Сумма строки № {i+1} равна {sum}");
+    }
+}
